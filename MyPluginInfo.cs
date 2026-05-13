845f35a9dcb8433d29a952a84f29fbc498b0e8b0
chlghksgml01 <chlghksgml01@naver.com>
2026-05-13 15:12:02 +0900

    Update PLUGIN_GUID to alloreandmushroominingmod

--- MyPluginInfo.cs
+++ MyPluginInfo.cs
@@ -2,7 +2,7 @@ namespace AllOreAndMushroomMiningMod;
 
 internal static class MyPluginInfo
 {
-    public const string PLUGIN_GUID    = "veinminingmod";
+    public const string PLUGIN_GUID    = "alloreandmushroominingmod";
     public const string PLUGIN_NAME    = "AllOreAndMushroomMiningMod";
     public const string PLUGIN_VERSION = "1.0.0";
 }
