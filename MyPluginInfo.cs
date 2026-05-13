4301b1e9cc6e40623ae29b9891258b200dd51c20
chlghksgml01 <chlghksgml01@naver.com>
2026-05-13 14:56:05 +0900

    Rename config file and GUID to veinminingmod, translate comments to English

--- MyPluginInfo.cs
+++ MyPluginInfo.cs
@@ -2,7 +2,7 @@ namespace VeinMiningMod;
 
 internal static class MyPluginInfo
 {
-    public const string PLUGIN_GUID    = "com.user.ft2.veinminingmod";
+    public const string PLUGIN_GUID    = "veinminingmod";
     public const string PLUGIN_NAME    = "VeinMiningMod";
     public const string PLUGIN_VERSION = "1.0.0";
 }
